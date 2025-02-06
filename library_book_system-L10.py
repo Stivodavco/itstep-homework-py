1b92f6597ed8c75312b06d32fa6042db970fbf22
Stivodavco <david.stepitacademy@gmail.com>
2025-02-06 18:06:01 +0100

    Finished class Kniznica

--- library_book_system-L10.py
+++ library_book_system-L10.py
@@ -23,3 +23,29 @@ class Kniznica:
 
     def pridat_knihu(self,kniha):
         self.zoznam_knih.append(kniha)
+
+    def vyhladat_knihu(self,nazov):
+        for kniha in self.zoznam_knih:
+            if kniha.nazov.lower() == nazov.lower():
+                return kniha
+        print("Kniha s nazvom \'" + nazov + "\' sa nenasla.")
+        return None
+
+    def vypozicat_knihu(self,isbn):
+        for kniha in self.zoznam_knih:
+            if kniha.isbn == isbn:
+                kniha.dostupna = False
+
+    def vratit_knihu(self,isbn):
+        for kniha in self.zoznam_knih:
+            if kniha.isbn == isbn:
+                kniha.dostupna = True
+
+    def zobrazit_zoznam_knih(self):
+        for kniha in self.zoznam_knih:
+            dostupnost = ""
+            if kniha.dostupna:
+                dostupnost = "Dostupna"
+            else:
+                dostupnost = "Nedostupna"
+            print("{} od {}, ISBN: {}, Rok: {}, {}".format(kniha.nazov,kniha.autor,kniha.isbn,kniha.rok_vydania,dostupnost))
